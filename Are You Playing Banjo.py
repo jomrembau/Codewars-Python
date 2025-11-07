45a42d7247a9b3058d2439ff31428f162745336e
jomrembau <jomir.bautista@gmail.com>
2025-11-07 09:23:45 +0100

    Add files via upload

---
+++ Are You Playing Banjo.py
@@ -0,0 +1,9 @@
+def are_you_playing_banjo(name):
+    letter_list = [x.lower() for x in name]
+    if letter_list[0] == "r":
+        return f"{name} plays banjo"
+    else:
+        return f"{name} does not play banjo"
+
+
+print(are_you_playing_banjo("martin"))
