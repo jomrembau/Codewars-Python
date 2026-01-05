9eec9ee73f2c1c8573f0f08cd79af34e6ddb3929
jomrembau <jomir.bautista@gmail.com>
2026-01-05 16:11:35 +0100

    Add files via upload

--- List Filtering.py
+++ List Filtering.py
@@ -4,9 +4,9 @@ current_list = [23,12,"zu", 4,1,"iol,45"]
 def filter_list(l):
     new_list = []
     for x in l:
-        if isinstance(x, int):
+        if type(x) == int:
             new_list.append(x)
-    return sorted(new_list)
+    return new_list
 
 print(filter_list(current_list))
 
