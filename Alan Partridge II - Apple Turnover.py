365793c8a96cc748f6dbf369c8b52348b698b430
jomrembau <jomir.bautista@gmail.com>
2026-04-14 13:55:47 +0200

    Add files via upload

---
+++ Alan Partridge II - Apple Turnover.py
@@ -0,0 +1,10 @@
+def apple(x):
+
+    x = int(x)
+
+    if x*x > 1000:
+        return "It's hotter than the sun!!"
+    else:
+        return "Help yourself to a honeycomb Yorkie for the glovebox."
+
+print(apple(5))
